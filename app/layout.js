f4b6d6c4aab4a75c3e942a6fcd356d226b1744c3
cosmicencounters5-png <cosmicencounters5@gmail.com>
2026-02-16 19:27:41 +0100

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,9 @@
 export default function RootLayout({ children }) {
   return (
     <html>
+      <head>
+        <meta name="p:domain_verify" content="0028c3e98d559ca3b0e6f5a0fe14eee8" />
+      </head>
       <body>{children}</body>
     </html>
   );
