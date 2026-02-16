7c4b0aaa496c80977edcff4184a6f5a184094712
cosmicencounters5-png <cosmicencounters5@gmail.com>
2026-02-16 19:34:56 +0100

    Update page.js

--- app/blog/[slug]/page.js
+++ app/blog/[slug]/page.js
@@ -1,11 +1,17 @@
+import { posts } from "@/app/lib/posts";
+
 export default function BlogPost({ params }) {
 
-  const { slug } = params;
+  const post = posts.find(p => p.slug === params.slug);
+
+  if (!post) {
+    return <h1>Not found</h1>;
+  }
 
   return (
     <main>
-      <h1>Blog post:</h1>
-      <p>{slug}</p>
+      <h1>{post.title}</h1>
+      <p>{post.content}</p>
     </main>
   );
 }
